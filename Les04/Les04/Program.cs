6d4dcdf7a8ca7503fa167238ff482fc3f6260b3b
Timo Zandbergen <zandbergentimo@gmail.com>
2021-11-07 12:24:24 +0100

    3 opdrachten af

--- Les04/Les04/Program.cs
+++ Les04/Les04/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace Les04
+namespace les4
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Room room1 = new Room(2, 2);
+        }
+    }
+
+    class Tiles
+    {
+    }
+    class Room
+    {
+        Tiles[][] tileMap;
+
+        public Room(int width, int height)
+        {
+            tileMap = new Tiles[height][];
+            for (int i = 0; i < tileMap.Length; i++)
+            {
+                tileMap[i] = new Tiles[width];
+                for (int K = 0; K < tileMap[i].Length; K++)
+                {
+                    tileMap[i][K] = new Tiles();
+                }
+            }
         }
     }
 }
